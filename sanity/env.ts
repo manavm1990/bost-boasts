33cce6356b8a80d4320c356020b8f6cc2c613b6b
Manav Misra <manavm@visionwebsoft.com>
2026-02-17 22:14:49 -0600

    refactor(sanity):

--- sanity/env.ts
+++ sanity/env.ts
@@ -12,7 +12,7 @@ export const projectId = assertValue(
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string) {
-  if (v === undefined) throw new Error(errorMessage);
+  if (!v) throw new Error(errorMessage);
 
   return v;
 }
