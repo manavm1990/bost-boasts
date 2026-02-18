108de6a6a4734a7681c3a4c36c1edfb1e9c135e7
Manav Misra <manavm@visionwebsoft.com>
2026-02-17 22:12:26 -0600

    refactor(sanity): :truck: don't append filename with '.types'
    It's already in the `types' directory.

--- sanity/types/index.ts
+++ sanity/types/index.ts
@@ -1,8 +1,8 @@
 import type { SchemaTypeDefinition } from "sanity";
-import { authorType } from "./author.types";
-import { blockContentType } from "./block-content.types";
-import { categoryType } from "./category.type";
-import { postType } from "./post.type";
+import { authorType } from "./author";
+import { blockContentType } from "./block-content";
+import { categoryType } from "./category";
+import { postType } from "./post";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [blockContentType, categoryType, postType, authorType],
