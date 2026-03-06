b7f8e4e5a3d4a719bfd7e43a09557fc28a4299e8
Manav Misra <manavm@visionwebsoft.com>
2026-03-05 20:08:31 -0600

    docs: :bulb:

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,6 +1,9 @@
 /**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
+ * This route is responsible for the built-in authoring environment using
+ * Sanity Studio.
+ *
+ * All routes under your studio path are handled by this file using
+ * Next.js' catch-all routes:
  * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
  *
  * You can learn more about the next-sanity package here:
