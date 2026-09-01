1ec0f2e8a6ca8ae43d87a534dbb0d99200c91eb2
Manav Misra <manavm@visionwebsoft.com>
2026-08-31 22:25:15 -0500

    chore: add development  for Sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,7 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({
   api: { projectId, dataset },
+  deployment: { appId: "yp2reu9o0wi4ddzya4vs482y" },
   typegen: {
     path: "./sanity/**/*.{ts,tsx}",
     schema: "./sanity/extract.json",
